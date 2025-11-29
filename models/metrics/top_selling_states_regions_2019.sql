4cdaaa78aa4ea55098682ef9c226ba4e1d5fb364
Guido Lorenzi <guido.lorenzi@jobylon.com>
2025-11-29 18:07:35 +0100

    Fix people seed to handle column names with spaces properly

--- models/metrics/top_selling_states_regions_2019.sql
+++ models/metrics/top_selling_states_regions_2019.sql
@@ -11,12 +11,12 @@ WITH orders_2019 AS (
 enriched AS (
     SELECT
         o.state_province,
-        o.Region as region,
+        o.region,
         p.`Regional Manager` as regional_manager,
         o.sales
     FROM orders_2019 AS o
     LEFT JOIN {{ ref('people') }} AS p
-        ON o.Region = p.Region
+        ON o.region = p.`Region`
 )
 
 SELECT
