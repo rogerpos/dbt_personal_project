d5dd42bae78767cb9ba0385d13d03084860b4fc9
guido <77778077+rogerpos@users.noreply.github.com>
2025-12-01 00:25:29 +0100

    schedule file (#5)

--- cloudrun/schedule.sh
+++ cloudrun/schedule.sh
@@ -3,16 +3,18 @@
 set -e
 
 PROJECT="data-gss"
-REGION="europe-north2"
+CLOUDRUN_REGION="europe-north2"
+SCHEDULER_REGION="europe-west1"
+SERVICE_NAME="load-to-bigquery"
 JOB_NAME="daily-load-to-bq"
-SERVICE_URL=$(gcloud run services describe load-to-bigquery --region $REGION --project $PROJECT --format="value(status.url)")
+SERVICE_URL=$(gcloud run services describe $SERVICE_NAME --region $CLOUDRUN_REGION --project $PROJECT --format="value(status.url)")
 
 echo "Creating Cloud Scheduler Job..."
 gcloud scheduler jobs create http $JOB_NAME \
   --schedule="0 6 * * *" \
   --uri="$SERVICE_URL" \
   --http-method=GET \
-  --location=$REGION \
+  --location=$SCHEDULER_REGION \
   --project=$PROJECT
 
 echo "Scheduler created! Will run daily at 06:00 UTC"
